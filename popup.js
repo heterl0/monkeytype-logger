61e5863b9cb6eb637e0421fc0929bb0e60bd4327
Hieu Le Van <lehieu99666@gmail.com>
2025-02-25 00:35:40 +0700

    Refactor popup UI and enhance last record display functionality

--- popup.js
+++ popup.js
@@ -1,14 +1,16 @@
 document.getElementById("downloadButton").addEventListener("click", () => {
   chrome.runtime.sendMessage({ action: "downloadRecords" });
-  window.close();
 });
 
 document.addEventListener("DOMContentLoaded", () => {
   chrome.storage.local.get(["records"], (result) => {
     const records = result.records || [];
     if (records.length > 0) {
-      document.getElementById("lastRecordContainer").textContent =
-        JSON.stringify(records[records.length - 1]);
+      document.getElementById("lastRecordContainer").textContent = records[
+        records.length - 1
+      ].words
+        .map((el) => el.word)
+        .join(", ");
     }
   });
 });
@@ -16,5 +18,17 @@ document.addEventListener("DOMContentLoaded", () => {
 document
   .getElementById("deleteLastRecordButton")
   .addEventListener("click", () => {
-    chrome.runtime.sendMessage({ action: "deleteLastRecord" });
+    chrome.runtime.sendMessage({ action: "deleteLastRecord" }, () => {
+      chrome.storage.local.get(["records"], (res) => {
+        const updatedRecords = res.records || [];
+        if (updatedRecords.length === 0) {
+          document.getElementById("lastRecordContainer").textContent = "";
+        } else {
+          document.getElementById("lastRecordContainer").textContent =
+            updatedRecords[updatedRecords.length - 1].words
+              .map((w) => w.word)
+              .join(", ");
+        }
+      });
+    });
   });
